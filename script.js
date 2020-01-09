8f117767d3a9e53f790d2ece4bb10699a52911b4
JuJu <58866597+jujubrains@users.noreply.github.com>
2020-01-09 15:46:22 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var secondsLeft = 10;
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+
+  }, 1000);
+}
