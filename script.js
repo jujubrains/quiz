63644c18ec2a03b8bb4fe54bd62502b615a67a1d
JuJu <58866597+jujubrains@users.noreply.github.com>
2020-01-10 03:17:10 -0500

    function click Answer, displayChoices, displayQUEST, Timer

--- script.js
+++ script.js
@@ -1,14 +1,107 @@
-var secondsLeft = 10;
 
-function setTime() {
+var countdownDisplay = document.getElementById("countdown");
+var startButton = document.getElementById("start");
+var displayQuestionEl = document.getElementById("title");
+var gameTimer = 100;
+var button1;
+var button2;
+var button3;
+var button4;
+var i=0;
+
+var questions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "Which is not one of words JSON stands for?",
+    choices: ["Java", "Script", "Jason", "Object"],
+    answer: "Jason"
+  },
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+];
+
+
+function timer() {
   var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft + "";
-
-    if(secondsLeft === 0) {
+    gameTimer--;
+    countdownDisplay.textContent=gameTimer;
+    if(gameTimer === 0)
+    // or if there are no more questions 
+    {
       clearInterval(timerInterval);
-      sendMessage();
+    //display score and show userInitial field.
     }
-
   }, 1000);
 }
+
+function displayQuest() {
+  displayQuestionEl.textContent=questions[i].title;
+}
+
+function displayChoices() {
+  button1.textContent = "1. "+questions[i].choices[0];
+  button2.textContent = "2. "+questions[i].choices[1];
+  button3.textContent = "3. "+questions[i].choices[2];
+  button4.textContent = "4. "+questions[i].choices[3];
+}
+
+startButton.addEventListener("click",function() {
+  event.preventDefault();
+  countdownDisplay.textContent=gameTimer;
+  timer();
+  displayQuest();
+  startButton.setAttribute("style","display:none");
+  document.getElementById("text").setAttribute("style","display:none");
+
+  button1 = document.createElement("button");
+  button1.textContent = "1. "+questions[i].choices[0];
+  button1.className = "button";
+  document.body.append(button1);
+  button2 = document.createElement("button");
+  button2.textContent = "2. "+questions[i].choices[1];
+  button2.className = "button";
+  document.body.append(button2);
+  button3 = document.createElement("button");
+  button3.textContent = "3. "+questions[i].choices[2];
+  button3.className = "button";
+  document.body.append(button3);
+  button4 = document.createElement("button");
+  button4.textContent = "4. "+questions[i].choices[3];
+  button4.className = "button";
+  document.body.append(button4);
+});
+
+function clickAnswer() {
+  getElementByClass("button").addEventListener("click",function(event) {
+    event.preventDefault();
+    if (event.target.textContent=questions[i].answer) {
+          i++;
+          displayQuest();
+          displayChoices();
+    }
+    else{
+          gameTimer-10;
+    }
+  })
+}
+
+console.log(countdownDisplay);
+console.log(questions[0].title);
+console.log(startButton);
